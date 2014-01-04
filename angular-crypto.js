4e3bb92605953dd9ea90e81b5888f4141d282535
gdi2290 <github@gdi2290.com>
2014-01-04 14:38:52 -0800

    fix init md5

--- angular-crypto.js
+++ angular-crypto.js
@@ -8,5 +8,5 @@
     angular.module("angular-crypto", [ "gdi2290.crypto" ]);
     angular.module("gdi2290.crypto-md5", []);
     angular.module("gdi2290.crypto-base64", []);
-    angular.module("gdi2290.crypto-md5").module("md5", function() {});
+    angular.module("gdi2290.crypto-md5").factory("md5", function() {});
 })(this, this.angular, this.Math, void 0);
