f5a202b75f33c4807a0154db1e16cc28561b0a59
gdi2290 <github@gdi2290.com>
2014-01-04 13:34:32 -0800

    add source file with min

---
+++ angular-crypto.js
@@ -0,0 +1,7 @@
+/*
+  Angular-Crypto - v0.0.1
+  2014-01-04
+*/
+(function(window, angular, Math) {
+    angular.module("gdi2290.crypto-md5").module("md5", function() {});
+})(this, this.angular, this.Math);
