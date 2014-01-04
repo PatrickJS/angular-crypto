46a64bf2d5af28b955a51fc90f535493b6d2944b
gdi2290 <github@gdi2290.com>
2014-01-04 13:46:18 -0800

    update source

--- angular-crypto.js
+++ angular-crypto.js
@@ -1,7 +1,11 @@
 /*
-  Angular-Crypto - v0.0.1
+  Angular-Crypto - v0.0.1 
   2014-01-04
 */
 (function(window, angular, Math) {
+    angular.module("angular-crypto", [ "gdi2290.crypto-md5", "gdi2290.crypto-base64" ]);
+    angular.module("ngCrypto", [ "angular-crypto" ]);
+    angular.module("gdi2290.crypto-md5", []);
+    angular.module("gdi2290.crypto-base64", []);
     angular.module("gdi2290.crypto-md5").module("md5", function() {});
 })(this, this.angular, this.Math);
