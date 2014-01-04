3a6eb18600a0e273bfbbe0ad35a363357cb1b967
gdi2290 <github@gdi2290.com>
2014-01-04 14:26:37 -0800

    add personal namespace for module name

--- angular-crypto.js
+++ angular-crypto.js
@@ -3,8 +3,9 @@
   2014-01-04
 */
 (function(window, angular, Math, undefined) {
-    angular.module("angular-crypto", [ "gdi2290.crypto-md5", "gdi2290.crypto-base64" ]);
-    angular.module("ngCrypto", [ "angular-crypto" ]);
+    angular.module("gdi2290.crypto", [ "gdi2290.crypto-md5", "gdi2290.crypto-base64" ]);
+    angular.module("ngCrypto", [ "gdi2290.crypto" ]);
+    angular.module("angular-crypto", [ "gdi2290.crypto" ]);
     angular.module("gdi2290.crypto-md5", []);
     angular.module("gdi2290.crypto-base64", []);
     angular.module("gdi2290.crypto-md5").module("md5", function() {});
